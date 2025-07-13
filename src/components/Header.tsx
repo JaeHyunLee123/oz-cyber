b2ed4a8fea002cf6d2d734d663d172af850086a5
JaeHyunLee123 <jhyon123@gmail.com>
2025-07-13 14:53:39 +0900

    #3 feat: responsive design to Header
    show Burger icon when widow width is smaller then 640px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,48 +1,60 @@
 import { Link, useLocation } from "react-router";
 import Cart from "./icons/Cart";
 import Heart from "./icons/Heart";
-import Logo from "./icons/logo";
+import Logo from "./icons/Logo";
 import User from "./icons/User";
 import SearchInput from "./SearchInput";
+import Burger from "./icons/Burger";
+import useWindowWidth from "../hooks/useWindowWidth";
 
 export default function Header() {
   const { pathname } = useLocation();
+  const windowWidth = useWindowWidth();
 
   return (
-    <header className="flex justify-around items-center p-10 space-x-3 flex-wrap">
+    <header className="flex justify-between px-2 sm:justify-around items-center py-10 space-x-3 flex-wrap">
       <Logo />
-      <SearchInput />
-      <nav className="flex justify-center items-center space-x-2">
-        <Link
-          to="/home"
-          className={pathname === "/" ? "text-black" : "text-gray-500"}
-        >
-          Home
-        </Link>
-        <Link
-          to="/about"
-          className={pathname === "/about" ? "text-black" : "text-gray-500"}
-        >
-          About
-        </Link>
-        <Link
-          to="/contact"
-          className={pathname === "/contact" ? "text-black" : "text-gray-500"}
-        >
-          Contanct Us
-        </Link>
-        <Link
-          to="/blog"
-          className={pathname === "/blog" ? "text-black" : "text-gray-500"}
-        >
-          Blog
-        </Link>
-      </nav>
-      <div className="flex justify-center items-center space-x-2">
-        <Heart />
-        <Cart />
-        <User />
-      </div>
+
+      {windowWidth >= 640 ? (
+        <>
+          <SearchInput />
+          <nav className="flex justify-center items-center space-x-2">
+            <Link
+              to="/home"
+              className={pathname === "/" ? "text-black" : "text-gray-500"}
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              className={pathname === "/about" ? "text-black" : "text-gray-500"}
+            >
+              About
+            </Link>
+            <Link
+              to="/contact"
+              className={
+                pathname === "/contact" ? "text-black" : "text-gray-500"
+              }
+            >
+              Contanct Us
+            </Link>
+            <Link
+              to="/blog"
+              className={pathname === "/blog" ? "text-black" : "text-gray-500"}
+            >
+              Blog
+            </Link>
+          </nav>
+          <div className="flex justify-center items-center space-x-2">
+            <Heart />
+            <Cart />
+            <User />
+          </div>
+        </>
+      ) : (
+        <Burger />
+      )}
     </header>
   );
 }
