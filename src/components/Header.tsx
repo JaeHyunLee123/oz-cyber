12d2d1be0e5f9b78ae1cce984f8e7bed06c173c4
JaeHyunLee123 <jhyon123@gmail.com>
2025-07-13 14:41:43 +0900

    #3 feat: Header UI

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,48 @@
+import { Link, useLocation } from "react-router";
+import Cart from "./icons/Cart";
+import Heart from "./icons/Heart";
+import Logo from "./icons/logo";
+import User from "./icons/User";
+import SearchInput from "./SearchInput";
+
 export default function Header() {
-  return <header>Header</header>;
+  const { pathname } = useLocation();
+
+  return (
+    <header className="flex justify-around items-center p-10 space-x-3 flex-wrap">
+      <Logo />
+      <SearchInput />
+      <nav className="flex justify-center items-center space-x-2">
+        <Link
+          to="/home"
+          className={pathname === "/" ? "text-black" : "text-gray-500"}
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className={pathname === "/about" ? "text-black" : "text-gray-500"}
+        >
+          About
+        </Link>
+        <Link
+          to="/contact"
+          className={pathname === "/contact" ? "text-black" : "text-gray-500"}
+        >
+          Contanct Us
+        </Link>
+        <Link
+          to="/blog"
+          className={pathname === "/blog" ? "text-black" : "text-gray-500"}
+        >
+          Blog
+        </Link>
+      </nav>
+      <div className="flex justify-center items-center space-x-2">
+        <Heart />
+        <Cart />
+        <User />
+      </div>
+    </header>
+  );
 }
