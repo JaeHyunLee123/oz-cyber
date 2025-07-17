a3f07f3a5cf2a441a998ee8c6050cb441dae2f38
JaeHyunLee123 <jhyon123@gmail.com>
2025-07-17 14:18:21 +0900

    #12 feat: fetch products by category

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,26 +1,89 @@
+import ProductCard from "@/components/ProductCard";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useState } from "react";
+
+const PRODUCT_API_BASE_URL = "https://dummyjson.com/products/category";
+
+type categories =
+  | "laptops"
+  | "mobile-accessories"
+  | "motorcycle"
+  | "smartphones"
+  | "tablets"
+  | "vehicle";
+
+interface BrowseItem {
+  category: categories;
+  text: string;
+}
+
+const browseItems: BrowseItem[] = [
+  {
+    category: "smartphones",
+    text: "Smart Phones",
+  },
+  {
+    category: "tablets",
+    text: "Tablets",
+  },
+  {
+    category: "laptops",
+    text: "Laptop",
+  },
+  {
+    category: "mobile-accessories",
+    text: "Mobile Accessories",
+  },
+  {
+    category: "vehicle",
+    text: "Vehicles",
+  },
+  {
+    category: "motorcycle",
+    text: "Motorcycles",
+  },
+];
 
 export default function Home() {
+  const [category, setCategory] = useState<categories>("smartphones");
+
   const { data, isPending } = useQuery<Products>({
-    queryKey: ["products", "smartphones"],
+    queryKey: ["products", category],
     queryFn: async () => {
       const data = await axios.get(
-        "https://dummyjson.com/products/category/smartphones?limit=8"
+        `${PRODUCT_API_BASE_URL}/${category}?limit=8&sortBy=rating&order=desc`
       );
 
-      console.log(data);
       return data.data;
     },
   });
 
   return (
     <div>
-      Home
-      <div>
+      <div className="p-1 flex items-center justify-start space-x-2">
+        {browseItems.map((item, i) => (
+          <button
+            key={i}
+            onClick={() => {
+              setCategory(item.category);
+            }}
+            className={`${
+              category === item.category
+                ? "text-black underline underline-offset-2"
+                : "text-gray-500"
+            } transition-colors hover:cursor-pointer hover:text-black`}
+          >
+            {item.text}
+          </button>
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-2">
         {isPending
           ? "fetching"
-          : data?.products.map((product) => <span>{product.title}</span>)}
+          : data?.products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
       </div>
     </div>
   );
