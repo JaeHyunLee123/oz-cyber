bb7e2d32e21357874b825604591c1d9dc902a095
JaeHyunLee123 <jhyon123@gmail.com>
2025-07-18 17:25:04 +0900

    #16 refactor: MOBILE_WIDTH constant

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,12 @@
 import useWindowWidth from "@/hooks/useWindowWidth";
 import DesktopHeader from "./DesktopHeader";
 import MobileHeader from "./MobileHeader";
+import { MOBILE_WIDTH } from "@/constants";
 
 export default function Header() {
   const windowWidth = useWindowWidth();
 
-  return <>{windowWidth >= 730 ? <DesktopHeader /> : <MobileHeader />}</>;
+  return (
+    <>{windowWidth >= MOBILE_WIDTH ? <DesktopHeader /> : <MobileHeader />}</>
+  );
 }
